27b893cbaeff831f0d348ce2214801d45e7eb586
fellyph <fellyph.cintra@gmail.com>
2019-02-02 18:12:58 +0000

    adding script

---
+++ src/index.js
@@ -0,0 +1,10 @@
+let footerElement = document.querySelector('footerbar')
+
+function component() {
+  let element = document.createElement('div');
+  element.innerHTML = 'Curso webpack';
+
+  return element;
+}
+
+footerElement.appendChild(component());
