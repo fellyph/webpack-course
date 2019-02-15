3d451fcf78eaf9dc01cbb0da84f9fe405f38aecd
fellyph <fellyph.cintra@gmail.com>
2019-02-15 16:59:37 +0000

    adding webpack clean plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,6 +3,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const common = require('./wepack.common');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -23,6 +24,12 @@ module.exports = merge(common, {
     ],
   },
   plugins: [
+    new CleanWebpackPlugin(['dist/css'],
+      {
+        exclude: ['dist/img'],
+      }),
+    new UglifyJsPlugin(),
+    new OptimizeCSSAssetsPlugin(),
     new MiniCssExtractPlugin({
       filename: '[name].[hash].css',
       chunkFilename: '[id].[hash].css',
