1c6877668f7e61dcdbb5abe3c074c3c6e45fb21f
fellyph <fellyph.cintra@gmail.com>
2019-02-15 14:46:21 +0000

    adding mini css extract

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   mode: 'development',
@@ -19,7 +20,7 @@ module.exports = {
       {
         test: /\.css$/,
         use: [
-          'style-loader',
+          MiniCssExtractPlugin.loader,
           {
             loader: 'css-loader',
             options: { importLoaders: 1 },
@@ -56,6 +57,10 @@ module.exports = {
       title: 'iMedia Cursos Brasil',
       template: './src/index.html',
       favicon: false,
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
     }),
   ],
 };
